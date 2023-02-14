403eb1c5e22c9dbb2c2ca34eda2703cfc9095c39
Pepitorino <carlos.ambrosio2007@gmail.com>
2023-02-15 06:06:02 +0800

    Skeleton for program

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    int x=1;
+
+    do 
+    {
+        dashboard();
+        destlm();
+        bucklm();
+        tripm();
+    } while(x);
+
+    return 0;
+}
